341a5e7e233ccc165278ba096a8adbeee6d5b971
danil.kruhlikov <danylo.kruhlikov@nure.ua>
2023-10-23 00:02:38 +0200

    pull

---
+++ amplify/.temp/#current-cloud-backend/function/orders/src/validation.js
@@ -0,0 +1,13 @@
+const Joi = require('joi');
+
+exports.validation = Joi.object({
+  id: Joi.string().required(),
+  product_type: Joi.string().required(),
+  email: Joi.string().required(),
+  fullName: Joi.string().required(),
+  package_id: Joi.string(),
+  phone: Joi.string().required(),
+  subscribe: Joi.boolean().required(),
+  currency: Joi.string().required(),
+  created_date: Joi.string().required(),
+});
--- src/pages/thank-you/index.tsx
+++ src/pages/thank-you/index.tsx
@@ -11,7 +11,7 @@ export default function ThankYouPage() {
     return <div className={css.root}>
         <div className={css.content}>
             <div className={css.title}>Дякую за замовлення</div>
-            <div className={css.text}>Незабаром вам надійде лист на пошту з доступом до навчальної платформи</div>
+            <div className={css.text}>Незабаром вам надійде лист на пошту з інформацією щодо підключення до трансляції</div>
             <div className={css.info}>Якщо лист не надійде протягом 12-ти годин, напишіть мені будь-ласка</div>
             <div className={css.links}>
                 <a href="https://t.me/bilanenco" className={css.link}>
