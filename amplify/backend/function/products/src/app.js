46834b4be68c4e4a62ab0dcde6de058459d219e5
danil.kruhlikov <danylo.kruhlikov@nure.ua>
2024-04-24 20:41:52 +0200

    update db permission

--- amplify/backend/function/products/src/app.js
+++ amplify/backend/function/products/src/app.js
@@ -1,4 +1,10 @@
-/*
+/* Amplify Params - DO NOT EDIT
+	ENV
+	REGION
+	STORAGE_PRODUCTS_ARN
+	STORAGE_PRODUCTS_NAME
+	STORAGE_PRODUCTS_STREAMARN
+Amplify Params - DO NOT EDIT *//*
 Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
     http://aws.amazon.com/apache2.0/
