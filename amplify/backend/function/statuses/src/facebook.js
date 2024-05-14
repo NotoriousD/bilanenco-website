6ee0ff914ab5cac796ba2b89995e238c86274d3d
danil.kruhlikov <danylo.kruhlikov@nure.ua>
2024-05-14 19:01:20 +0200

    add pixel

--- amplify/backend/function/statuses/src/facebook.js
+++ amplify/backend/function/statuses/src/facebook.js
@@ -1,13 +1,37 @@
 const adsSdk = require('facebook-nodejs-business-sdk');
+const axios = require('axios');
 const Content = adsSdk.Content;
 const CustomData = adsSdk.CustomData;
 const EventRequest = adsSdk.EventRequest;
 const UserData = adsSdk.UserData;
 const ServerEvent = adsSdk.ServerEvent;
 
+const httpClientFB = axios.create({
+    baseURL: 'https://graph.facebook.com/v19.0',
+});
+
+const clientId = '648513670787345';
+const clientSecret = 'b622c1032b4f19f55909cdfac5bd340b';
 const access_token = 'EAAJN0dr6sREBO3BZBWbky7hV9mUpqwBJjZAeAj4nqjqAuzF7nlTi9SOBk3ZAMXYimkBqC22LSOf1gc6b5sEq56yIixY5LVcJ7UgC6pqU6n4IlAIMfpZBkJX7iBMCAmELKJb26i4H88WLUhLXNz38v2jhfDt1ZCRWZBO5k5cDZC3SWyCtX6X8vfq6xIj9v1yygwsR1Mc6MOTC11di1KzJQ6WZAsPcvac34mJStRDLLCdZABjZCP2LHbGuoB5vds1lgTCgZDZD';
 const pixel_id = '456894110068958';
 const api = adsSdk.FacebookAdsApi.init(access_token);
+
+exports.fbAuth = async () => {
+    try {
+        const { data } = await httpClientFB.get('/oauth/access_token', {
+            params: {
+                client_id: clientId,
+                client_secret: clientSecret,
+                grant_type: "client_credentials"
+            }
+        })
+
+
+        return data.access_token;
+    } catch(e) {
+        console.log(e);
+    }
+}
 
 exports.sendPurchaseEvent = async ({
     email,
@@ -16,38 +40,44 @@ exports.sendPurchaseEvent = async ({
     price
 }) => {
 
-    let current_timestamp = Math.floor(new Date() / 1000);
-
-    const userData = (new UserData())
-        .setEmails([email])
-
-    const content = (new Content())
-        .setId(productName)
-        .setQuantity(1);
-
-    const customData = (new CustomData())
-        .setContents([content])
-        .setCurrency(currency)
-        .setValue(price);
-
-    const serverEvent = (new ServerEvent())
-        .setEventName('Purchase')
-        .setEventTime(current_timestamp)
-        .setUserData(userData)
-        .setCustomData(customData)
-        .setEventSourceUrl('http://bilanenco.com/products/inspobook-2024-summer')
-        .setActionSource('website');
-
-    const eventsData = [serverEvent];
-    const eventRequest = (new EventRequest(access_token, pixel_id))
-        .setEvents(eventsData);
-
     try {
-        const response = await eventRequest.execute();
+        const accessToken = await this.fbAuth();
 
-        console.log(response);
-    } catch(e) {
-        console.log('error', e);
+        console.log(accessToken);
+
+        let current_timestamp = Math.floor(new Date() / 1000);
+    
+        const userData = (new UserData())
+            .setEmails([email])
+    
+        const content = (new Content())
+            .setId(productName)
+            .setQuantity(1);
+    
+        const customData = (new CustomData())
+            .setContents([content])
+            .setCurrency(currency)
+            .setValue(price);
+    
+        const serverEvent = (new ServerEvent())
+            .setEventName('Purchase')
+            .setEventTime(current_timestamp)
+            .setUserData(userData)
+            .setCustomData(customData)
+            .setEventSourceUrl('http://bilanenco.com/products/inspobook-2024-summer')
+            .setActionSource('website');
+    
+        const eventsData = [serverEvent];
+        const eventRequest = (new EventRequest(accessToken, pixel_id))
+            .setEvents(eventsData);
+    
+        eventRequest.execute().then((response) => {
+            console.log(response);
+        }).catch((err) => {
+            console.log('error', err);
+        })
+    } catch (e) {
+        console.log('err', e);
     }
 }
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,22 @@ export default function Document() {
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
                 <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
                 <meta name="facebook-domain-verification" content="0tx6527zm4yikhbnxozkw68arc2m5x" />
+                <script dangerouslySetInnerHTML={{
+                    __html: `!function(f,b,e,v,n,t,s)
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}(window, document,'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+                    fbq('init', '456894110068958');
+                    fbq('track', 'PageView');` }}
+                />
+                <noscript dangerouslySetInnerHTML={{
+                    __html: `<img height="1" width="1" style="display:none"
+                    src="https://www.facebook.com/tr?id=456894110068958&ev=PageView&noscript=1" />` }}
+                />
             </Head>
             <body>
                 <Main />
